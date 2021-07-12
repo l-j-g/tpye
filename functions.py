4f3122acfa52b06c7a9bb85b6b9b163c01d720f9
lachnloaded <lachlang93@gmail.com>
2021-07-12 20:52:54 +1000

    skeleton for welcome function and start function

--- functions.py
+++ functions.py
@@ -1,3 +1,12 @@
+from art import *
+
 def welcome():
-	print("tpye")
+	tprint("tpye",font="block",chr_ignore=True)
+	selection = input("(a)bout, (s)tart, (h)ighscores\n")
+	return selection
+
+def start():
+	user_name = input("Enter your name: ")
+	difficulty = input("Choose your difficulty: (b): beginner, (i): intermediate, (e): expert\n")
+	if difficulty ""
 	return
