f5a1066e279da9fca1652d9e67a264017e446a37
lachnloaded <lachlang93@gmail.com>
2021-07-12 22:20:29 +1000

    add function to select test text based on user difficulty

--- functions.py
+++ functions.py
@@ -1,12 +1,25 @@
 from art import *
+from content import *
+from random import randrange
 
 def welcome():
 	tprint("tpye",font="block",chr_ignore=True)
 	selection = input("(a)bout, (s)tart, (h)ighscores\n")
 	return selection
 
+def about():
+	pass
+
+def highscores():
+	pass
+
 def start():
 	user_name = input("Enter your name: ")
 	difficulty = input("Choose your difficulty: (b): beginner, (i): intermediate, (e): expert\n")
-	if difficulty ""
-	return
+	if difficulty in difficulties:
+		# Select the test text for the selected difficulty 
+		tests = typing_tests[difficulties[difficulty]]
+		# Choose a random passage of text of the selected difficulty 
+		total_tests = len(tests)
+		test_txt = tests[randrange(1,total_tests,1)]
+		print(test_txt)
